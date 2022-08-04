116e6f38ab7ce29df4a2a7830d9a5988b97fddd5
rcc0404 <rahulchaudhar3@gmail.com>
2022-08-04 14:42:49 +0530

    Thread Exucation

--- block-BJaagm/index.js
+++ block-BJaagm/index.js
@@ -1,25 +1,5 @@
-1. What does thread of execution means in JavaScript?
-
-2. Where the JavaScript code gets executed?
-
-3. What does context means in Global Execution Context?
-
-4. When do you create a global execution context.
-
-5. Execution context consists of what all things?
-
-6. What are the different types of execution context?
-
-7. When global and function execution context gets created?
-
-8. Function execution gets created during function execution or while declaring a function.
-
-
-9. Create a execution context diagram of the following code on your notebook. Take a screenshot/photo and store it in the folder named `img`. Use `![](./img/image-name.png)` to display it here.
-
-
-
-```js
+// 1. What does thread of execution means in JavaScript ?
+//Ans:-    
 var user = "Arya";
 
 function sayHello(){
@@ -27,9 +7,49 @@ function sayHello(){
 }
 
 var userMsg = sayHello(user);
-```
 
-<!-- Put your image here -->
+// 2. Where the JavaScript code gets executed?
+
+//Ans:-JavaScript Engine
+
+// 3. What does context means in Global Execution Context?
+
+//Ans:-Environment means where code is executed.
+
+// 4. When do you create a global execution context.
+
+//Ans:- Global execution context will be created in the start.
+
+// 5. Execution context consists of what all things?
+
+//Ans:- Two thing One- memory which is store in var/function
+//Second - operation/execution/manipulation
+
+// 6. What are the different types of execution context?
+
+//Ans:- Global execution & function execution context
+
+// 7. When global and function execution context gets created?
+
+// Ans:- Global execution context will be created in Start
+// Function execution context will be created in when function execution.
+
+// 8. Function execution gets created during function execution or while declaring a function.
+
+//Ans:- execution
+
+// 9. Create a execution context diagram of the following code on your notebook. Take a screenshot/photo and store it in the folder named `img`. Use `![](./img/image-name.png)` to display it here.
+
+var user = "Arya";
+
+function sayHello(){
+  return `Hello ${user}`;
+}
+
+var userMsg = sayHello(user);
+
+
+// <!-- Put your image here -->
 
 ![](./img/image-name.jpg)
 
